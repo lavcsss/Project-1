3bebbeedcbb8dfda3db889ca93c9c94d02ddec04
lavc4 <lav@sourcesoftsolutions.com>
2022-03-09 14:01:27 +0530

    bmi and age calculator

---
+++ Bmi_calculator.py
@@ -0,0 +1,5 @@
+# BMI calculator
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+bmi = int(int(weight )/ (height) ** 2)
+print('bmi: ', bmi)
---
+++ age_calcualtor.py
@@ -0,0 +1,4 @@
+# calculating the life left
+age = input("Enter your age")
+life_left = 90 - int(age)
+days = life_left * 365
