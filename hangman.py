4592a069f09a250ab8f0627c2d3aef5bf4eb3317
lavc4 <lav@sourcesoftsolutions.com>
2022-03-22 20:58:07 +0530

    modified hangman

--- hangman.py
+++ hangman.py
@@ -2,16 +2,21 @@ import random
 # Hangman game 
 # Take a list for of words.
 
-
+guessed_word = []
 words = ["player", "happy", "racer", "coder"]
 word = random.choice(words)
 print(word)
 for i in range(0,len(word)):
-    print("_", end= " ")
+    guessed_word.append("_")
+print(guessed_word)
 for i in word:
     guess_letter = input("\nGuess the letter:")
     if word.count(guess_letter) > 0:
-        print(word[word.index(guess_letter)])
-        
-    else:
+        for j in range(0, len(word)):
+            if guess_letter == word[j]:
+                guessed_word[j] = word[word.index(guess_letter)]
+        else:
+            pass
+    print(guessed_word)
+else:
         print("Better luck next time")
