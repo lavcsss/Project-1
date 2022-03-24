e50c017ccf0e1f7199e7e93d4160929004236a44
lavc4 <lav@sourcesoftsolutions.com>
2022-03-24 19:47:24 +0530

    Final submit of hangman game

--- hangman.py
+++ hangman.py
@@ -1,34 +1,43 @@
 import random
 import hangman_arts
-import hangman_words
+# Update the word list to use the 'word_list' from hangman_words.py
+from hangman_words import words
+
 # Hangman game 
-# Take a list for of words.
 print(hangman_arts.logo)
 lives = -1
 flag = False
 guessed_word = []
+
 # words = ["player", "happy", "racer", "coder"]
-word = random.choice(hangman_words.words)
-print(word)
+word = random.choice(words)
+
+#Create blanks
 for i in range(0,len(word)):
     guessed_word.append("_")
-print(guessed_word)
+print(f"{' '.join(guessed_word)}")
+
 while not flag:
-    guess_letter = input("\nGuess the letter:")
+    guess_letter = input("\nGuess the letter:").lower()
+    if guess_letter in guessed_word:
+        print(f"You already gussed {guess_letter}")
     if word.count(guess_letter) > 0:
+
+        #Check guessed letter
         for j in range(len(word)):
-            if guess_letter == guessed_word[j]:
-                print(f"You already gussed {guess_letter}")
+        # If the user has entered a letter they've already guessed, print the letter and let them know.
             if guess_letter == word[j]:
                 guessed_word[j] = word[word.index(guess_letter)]
     else:
-        print("Better luck next time")
+        # If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"{guess_letter} is not correct letter. Better luck next time")
         print(hangman_arts.stages[lives])
         lives -= 1
-    print(guessed_word)
+    print(f"{' '.join(guessed_word)}")
     if "_" not in guessed_word:
         flag = True
         print("You win")
     if lives == -8:
         flag = True
         print("You lose")
+        print(f" Ans is {word}")
